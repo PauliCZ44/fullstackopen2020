752eaec86d66deefa4fdc35b155bec9b73b77827
PauliCZ44 <stastnyp@seznam.cz>
2020-08-21 09:42:37 +0200

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,78 +1,102 @@
-import React, { useState } from 'react'
-import PersonForm from "./components/PersonForm"
-import Persons from "./components/Persons"
-import Filter from "./components/Filter"
+import React, { useState, useEffect } from "react";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+import Filter from "./components/Filter";
+import axios from "axios";
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '123-456-789'},
-    { name: 'Arto Pavel', number: '553-456-789'  }
-  ]) 
-  const [ newName, setNewName ] = useState('Add name here')
-  const [ newNumber, setNewNumber ] = useState('')
-  const [ filterName, setFilterName ] = useState('')
-  
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("Add name here");
+  const [newNumber, setNewNumber] = useState("");
+  const [filterName, setFilterName] = useState("");
+
+    
+  useEffect(() => {
+    console.log("effect")
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        //console.log('promise fulfilled')
+        setPersons(response.data)
+      });
+  }, [])
+  console.log('render', persons.length, 'persons')
+
+
+
   const handleChangeName = (e) => {
-      console.log(e.target.value)
-      setNewName(e.target.value)
-  }
+    //console.log(e.target.value)
+    setNewName(e.target.value);
+  };
   const handleChangeNumber = (e) => {
-    console.log(e.target.value)
-    setNewNumber(e.target.value)
-  }
-    const handleChangeFilter = (e) => {
-    console.log(e.target.value)
-    setFilterName(e.target.value)
-  }
+    //console.log(e.target.value)
+    setNewNumber(e.target.value);
+  };
+  const handleChangeFilter = (e) => {
+    //console.log(e.target.value)
+    setFilterName(e.target.value);
+  };
 
   const addName = (e) => {
-    e.preventDefault()
-    //loo at at the array of person and if there is not a name like newName then OK, else alert
-    let newPerson = {name: newName, number: newNumber}
-    if (persons.filter(x => x.name === newName).length > 0) {
-      alert(`${newName} is already added to phonebook`)   //alt 96  or alt+``
+    e.preventDefault();
+    //look at at the array of person and if there is not a name like newName then OK, else alert
+    let newPerson = { name: newName, number: newNumber };
+    if (persons.filter((x) => x.name === newName).length > 0) {
+      alert(`${newName} is already added to phonebook`); //alt 96  or alt+``
     } else {
-    setPersons(persons.concat(newPerson));
-    console.log("Person", persons)
-    setNewName("")
-  }
-  }
+      setPersons(persons.concat(newPerson));
+      // console.log("Person", persons)
+      setNewName("");
+    }
+  };
 
   let searchInString = (str, subStr) => {
-  console.log(str, subStr)
-  if (str.indexOf(subStr) >= 0 ) {
-  return true
+    //console.log(str, subStr)
+    if (str.indexOf(subStr) >= 0) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  //console.log(searchInString(persons[0].name.toLowerCase(), filterName.toLowerCase()))
+  let personsList = [];
+  if (filterName !== "") {
+    //když je filtr zaplý
+    console.log("FilterOn");
+    // console.log(persons.filter( x => searchInString(x.name.toLowerCase(), filterName.toLowerCase())))
+    personsList = persons
+      .filter((x) =>
+        searchInString(x.name.toLowerCase(), filterName.toLowerCase())
+      )
+      .map((x) => (
+        <li key={x.name}>
+          {x.name} {x.number}
+        </li>
+      ));
   } else {
-    return false
+    //console.log("Filter OFF")
+    personsList = persons.map((x) => (
+      <li key={x.name}>
+        {x.name} {x.number}
+      </li>
+    ));
   }
-} 
-//console.log(searchInString(persons[0].name.toLowerCase(), filterName.toLowerCase()))
-let personsList = []
-if ( filterName  !== "" ) {
-  //když je filtr zaplý
-  console.log("FilterOn")
-  // console.log(persons.filter( x => searchInString(x.name.toLowerCase(), filterName.toLowerCase())))
-personsList = persons.filter( x => searchInString(x.name.toLowerCase(), filterName.toLowerCase())).map(x => <li key={x.name}>{x.name} {x.number}</li>)
-} else {
-  console.log("Filter OFF")
-personsList = persons.map(x => <li key={x.name}>{x.name} {x.number}</li>)
-}
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter handleChangeFilter={handleChangeFilter}  filterName={filterName}/>
+      <Filter handleChangeFilter={handleChangeFilter} filterName={filterName} />
 
-      <PersonForm 
-      newName={newName} 
-      handleChangeName={handleChangeName} 
-      newNumber={newNumber} 
-      handleChangeNumber={handleChangeNumber}
-      addName={addName}
+      <PersonForm
+        newName={newName}
+        handleChangeName={handleChangeName}
+        newNumber={newNumber}
+        handleChangeNumber={handleChangeNumber}
+        addName={addName}
       />
 
-      <Persons list={personsList}/>
+      <Persons list={personsList} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part2/phonebook/src/components/Filter.js
+++ part2/phonebook/src/components/Filter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Filter = (props) => {
-    console.log(props, "props in filer")
+    //console.log(props, "props in filer")
     return (
         <div>
         Filter by name: <input value={props.filterName} placeholder="Filter..." onChange={props.handleChangeFilter}/>
