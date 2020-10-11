fd61112eba4d7838c8fc51659e7e6d765c80271f
PauliCZ44 <stastnyp@seznam.cz>
2020-10-11 15:56:38 +0200

    4.2 Blog list, step2

---
+++ part4/controllers/blogs.js
@@ -0,0 +1,34 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+    Blog
+      .find({})
+      .then(blogs => {
+        response.json(blogs)
+      })
+  })
+  
+blogsRouter.post('/', (request, response) => {
+    const blog = new Blog(request.body)
+  
+    blog
+      .save()
+      .then(result => {
+        response.status(201).json(result)
+      })
+  })
+
+ blogsRouter.get('/:id', (req, res, next) => {
+     Blog.findById(req.params.id)
+        .then(blog => {
+        if(blog) {
+            res.json(blog)
+        } else {
+            res.status(404).end()
+        }
+     })
+     .catch(err => next(err))
+ })
+
+  module.exports = blogsRouter
