c24ead57d71b8b9ef2797225fea6bcdd98426941
PauliCZ44 <stastnyp@seznam.cz>
2020-10-17 09:17:30 +0200

    4.14 Blog list expansions, step2

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+//const { response } = require('../app')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
@@ -32,6 +33,36 @@ blogsRouter.get('/:id', (req, res, next) => {
       }
     })
     .catch(err => next(err))
+})
+
+blogsRouter.delete('/:id', async (req, res, next) => {
+  try {
+    await Blog.findByIdAndRemove(req.params.id)
+    res.status(204).end()
+    console.log('blog was deleted')
+  } catch(exception) {
+    next(exception)
+  }
+})
+
+
+blogsRouter.put('/:id', async (req, res, next) => {
+  const blogToBeChanged = await Blog.findById(req.params.id)
+  //console.log("blogToBeChanged", blogToBeChanged)
+  const body = req.body
+  //console.log("Body", body)
+  const blogToBeSaved = {
+    author: body.author ? body.author : blogToBeChanged.author,
+    title: body.title ? body.title : blogToBeChanged.title,
+    url: body.url ? body.url : blogToBeChanged.url,
+    likes: body.likes
+  }
+  try {
+    await Blog.findByIdAndUpdate(req.params.id, blogToBeSaved,  { new: true })
+    res.sendStatus(200)
+  } catch(exception) {
+    next(exception)
+  }
 })
 
 module.exports = blogsRouter
--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -9,8 +9,6 @@ const api = supertest(app)
 
 const Blog = require('../models/blog')
 
-
-
 const initialBlogs = helper.listWithTwoBlogs
 
 beforeEach(async () => {
@@ -23,91 +21,131 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-test('API - blogs are returned as json and are 2 (4.8)', async () => {
-  const result = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-  expect(result.body).toHaveLength(initialBlogs.length)
-})
-
-test('ID is not undefined (4.9)', async () => {
-
-  const blogs = await api.get('/api/blogs')
-  //console.log(blogs.body)
-  blogs.body.map(blog => {
-    expect(blog.id).toBeDefined()
+describe('GET method', () => {
+  test('API - blogs are returned as json and are 2 - 4.8', async () => {
+    const result = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+    expect(result.body).toHaveLength(initialBlogs.length)
   })
 })
 
-test('POST is working (4.10)', async () => {
-  let initialBlogs = await blogsInDb()
-
-  const newBlog =  {
-    title: 'First class tests',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-    likes: 10
-  }
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  let finalBlogs = await blogsInDb()
-  //console.log("log in test", finalBlogs, "initial:", initialBlogs)
-  //total number of vlogs is increased by one
-  expect(initialBlogs.length).toBe(finalBlogs.length-1)
-
-  //test that on DB is content of new blog
-  let titles = finalBlogs.map(blog => blog.title)
-  expect(titles).toContain(newBlog.title)
+describe('DB Structure', () => {
+  test('ID is not undefined - 4.9', async () => {
+    const blogs = await api.get('/api/blogs')
+    //console.log(blogs.body)
+    blogs.body.map(blog => {
+      expect(blog.id).toBeDefined()
+    })
+  })
 })
 
-test('POST blog with undefined likes has 0 likes and is added', async () => {
-  let initialBlogs = await blogsInDb()
-  const blogToBePosted =  {
-    title: 'Post without likes',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions2.html',
-  }
+describe('POST methods', () => {
+  test('POST is working - 4.10', async () => {
+    let initialBlogs = await blogsInDb()
 
-  await api
-    .post('/api/blogs')
-    .send(blogToBePosted)
-    .expect(201)
+    const newBlog =  {
+      title: 'First class tests',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+      likes: 10
+    }
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
 
-  let newBlogList = await blogsInDb()  //fetch array of blogs in DB
-  let hasZeroLikes = newBlogList[newBlogList.length-1]  //get the newest one
-  expect(hasZeroLikes.likes).toBe(0)    // it has 0 likes
-  expect(newBlogList.length).toBe(initialBlogs.length+1)
+    let finalBlogs = await blogsInDb()
+    //console.log("log in test", finalBlogs, "initial:", initialBlogs)
+    //total number of vlogs is increased by one
+    expect(initialBlogs.length).toBe(finalBlogs.length-1)
+
+    //test that on DB is content of new blog
+    let titles = finalBlogs.map(blog => blog.title)
+    expect(titles).toContain(newBlog.title)
+  })
+
+  test('POST blog with undefined *LIKES* has 0 likes and is added - 4.11*', async () => {
+    let initialBlogs = await blogsInDb()
+    const blogToBePosted =  {
+      title: 'Post without likes',
+      author: 'Robert C. Martin',
+      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions2.html',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blogToBePosted)
+      .expect(201)
+
+    let newBlogList = await blogsInDb()  //fetch array of blogs in DB
+    let hasZeroLikes = newBlogList[newBlogList.length-1]  //get the newest one
+    expect(hasZeroLikes.likes).toBe(0)    // it has 0 likes
+    expect(newBlogList.length).toBe(initialBlogs.length+1)
+  })
+
+  test('POST blog with undefined *URL* is not added - 4.12*', async () => {
+    const blogToBePosted =  {
+      title: '22Test2TAitle',
+      author: 'Robert C. Martin'
+    }
+    await api
+      .post('/api/blogs')
+      .send(blogToBePosted)
+      .expect(400)
+    let finalBlogs = await blogsInDb()
+    expect(initialBlogs.length).toBe(finalBlogs.length)
+  })
+
+  test('POST blog with undefined *TITLE* is not added - 4.12*', async () => {
+    const blogToBePosted =  {
+      url: 'wwww.12345.com',
+      author: 'Another C. Martin'
+    }
+    await api
+      .post('/api/blogs')
+      .send(blogToBePosted)
+      .expect(400)
+    let finalBlogs = await blogsInDb()
+    expect(initialBlogs.length).toBe(finalBlogs.length)
+  })
 })
 
-test('POST blog with undefined URL is not added', async () => {
-  const blogToBePosted =  {
-    title: '22Test2TAitle',
-    author: 'Robert C. Martin'
-  }
-  await api
-    .post('/api/blogs')
-    .send(blogToBePosted)
-    .expect(400)
-  let finalBlogs = await blogsInDb()
-  expect(initialBlogs.length).toBe(finalBlogs.length)
+describe('DELETE', () => {
+  test('DELETE is working 4.13', async () => {
+    let initialBlogs = await blogsInDb()
+    let adress = '/api/blogs/' + initialBlogs[1]['id']
+    // console.log('adress = ', adress)
+    await api
+      .delete(adress)
+      .expect(204)
+
+    let newBlogList = await blogsInDb()  //fetch array of blogs in DB
+    expect(newBlogList.length).toBe(initialBlogs.length-1)
+  })
 })
 
-test('POST blog with undefined TITLE is not added', async () => {
-  const blogToBePosted =  {
-    url: 'wwww.12345.com',
-    author: 'Another C. Martin'
-  }
-  await api
-    .post('/api/blogs')
-    .send(blogToBePosted)
-    .expect(400)
-  let finalBlogs = await blogsInDb()
-  expect(initialBlogs.length).toBe(finalBlogs.length)
+describe('PUT', () => {
+  test('PUT is working 4.14', async () => {
+    let initialBlogs = await blogsInDb()
+    let adress = '/api/blogs/' + initialBlogs[1]['id']
+    let randLikes = Math.floor(Math.random()*100)
+    let newBlog = {
+      author: 'Test AUthor with PUT',
+      likes: randLikes
+    }
+    // console.log('adress = ', adress)
+    await api
+      .put(adress)
+      .send(newBlog)
+      .expect(200)
+
+    let newBlogList = await blogsInDb()  //fetch array of blogs in DB
+    expect(newBlogList.length).toBe(initialBlogs.length)
+    expect(newBlogList[1]['likes']).toBe(randLikes)
+  })
 })
 
 
